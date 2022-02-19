2fe3934666401c2d7061de26fe105edf9abaf84f
Anup <anupleo5@gmail.com>
2022-02-19 07:39:55 +0000

    changes

--- Models/Player.cs
+++ Models/Player.cs
@@ -5,14 +5,11 @@ using PubgTournament.Models;
 namespace PubgTournament.Models
 {
     public class Player : Document{
-        // Player(){
-        //     Kills=0;
-        //     IsAlive=false;
-        //     IsSub=false;
-        // }
+       
         public string PlayerName { get; set; }
         public int Kills { get; set; }
         public bool IsAlive { get; set; }
         public bool IsPlaying { get; set; }
+        public bool Domination { get; set; }
 }
 }
