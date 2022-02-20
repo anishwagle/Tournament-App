819826f7998843390bec802bce7d68e946c6513e
anish wagle <anish.wagle@bhattaraisoftwareinc.com>
2022-02-20 09:20:11 +0545

    updated model

--- Models/Team.cs
+++ Models/Team.cs
@@ -4,7 +4,8 @@ namespace PubgTournament.Models
 {
     public class Team : Document{
         public string FullName { get; set; }
-        public byte[] TeamLogo { get; set; }
+
+        public string TeamLogo { get; set; }
         public string ShortName { get; set; }
         public List<Player> Players { get; set; }
         public int Kills { get; set; }
